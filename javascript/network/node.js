70430a0cc19c3216e2d73ea2d1ec74dc64b65efa
Chris Strom <eee.c@eeecooks.com>
2010-08-24 22:51:51 -0400

    Warn and debug message reception exceptions

--- javascript/network/node.js
+++ javascript/network/node.js
@@ -7,30 +7,34 @@ Faye.NodeHttpTransport = Faye.Class(Faye.Transport, {
         response = null,
         retry    = this.retry(message, timeout),
         self     = this;
-    
+
     client.addListener('error', retry);
-    
+
     client.addListener('end', function() {
       if (!response) retry();
     });
-    
+
     var request = client.request('POST', uri.pathname, {
       'Content-Type':   'application/json',
       'Host':           uri.hostname,
       'Content-Length': content.length
     });
-    
+
     request.addListener('response', function(stream) {
       response = stream;
       Faye.withDataFor(response, function(data) {
         try {
           self.receive(JSON.parse(data));
         } catch (e) {
+          var stack = e.stack.split("\n");
+          self.error(stack[0] +  " " + stack[1].replace(/ +/, ''));
+          self.debug(e.stack);
+
           retry();
         }
       });
     });
-    
+
     request.write(content);
     request.end();
   }
